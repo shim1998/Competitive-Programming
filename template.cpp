4f58a84b4c6f859324fe18e512c71cf1eb989ac3
Shivam Solanki <98shim@gmail.com>
2020-04-02 10:50:32 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,56 @@
+/*Made by Shivam Solanki*/
+#include<bits/stdc++.h>
+using namespace std;
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define ll long long int
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vll> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+typedef vector<pii> vp;
+typedef vector<pll> vpll;
+typedef map<int,int> mii;
+typedef map<ll,ll> mll;
+typedef set<int> sii;
+typedef set<ll> sll;
+typedef priority_queue<int> pq;
+typedef unordered_map<int,int> umii;
+typedef unordered_map<ll,ll> umll;
+typedef queue<int> qii;
+#define rep(i,k,n) for (int i = k; i < n; ++i) 
+#define repr(i,k,n) for (int i = n; i>=k; --i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i) 
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+#define repll(i,k,n) for (ll i = k; i < n; ++i) 
+#define pb push_back
+#define mp make_pair
+#define gcd __gcd
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const int INF = 1e9+5;
+const int MOD = 1e9+7;
+
+int solve(){
+    return 0;
+}
+    
+int main(){
+    // #ifndef ONLINE_JUDGE  
+        // freopen("input.txt", "r", stdin); 
+        // freopen("output.txt", "w", stdout); 
+    // #endif 
+    // fastio;
+    int t=1;
+    cin>>t;
+    int i=1;
+    while(t--){
+        cout<<"Case #"<<i<<": "<<solve()<<'\n';
+        i++;
+    }
+    return 0;
+}
