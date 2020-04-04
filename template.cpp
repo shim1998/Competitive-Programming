c12068472eb2d35dfb88e78cda1df6f99cd8d5ee
Shim1998 <98shim@gmail.com>
2020-04-04 13:04:28 +0530

    Final templates

--- template.cpp
+++ template.cpp
@@ -19,48 +19,27 @@ typedef set<ll> sll;
 typedef priority_queue<int> pq;
 typedef unordered_map<int,int> umii;
 typedef unordered_map<ll,ll> umll;
-<<<<<<< HEAD
 #define all(x) x.begin(),x.end()
 #define rep(i,k,n) for (int i = k; i < n; ++i) 
 #define repr(i,k,n) for (int i = n; i>=k; --i)
-=======
-typedef queue<int> qii;
-#define rep(i,k,n) for (int i = k; i < n; ++i) 
-#define repr(i,k,n) for (int i = n; i>=k; --i)
-#define REP(i,k,n) for (int i = k; i <= n; ++i) 
-#define REPR(i,k,n) for (int i = k; i >= n; --i)
->>>>>>> 4f58a84b4c6f859324fe18e512c71cf1eb989ac3
 #define repll(i,k,n) for (ll i = k; i < n; ++i) 
 #define pb push_back
 #define mp make_pair
 #define gcd __gcd
 #define F first
 #define S second
-<<<<<<< HEAD
-=======
-#define all(x) x.begin(),x.end()
->>>>>>> 4f58a84b4c6f859324fe18e512c71cf1eb989ac3
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 const int INF = 1e9+5;
 const int MOD = 1e9+7;
 
-<<<<<<< HEAD
 void solve(){
 }
     
 int main(){ 
-=======
-int solve(){
-    return 0;
-}
-    
-int main(){
->>>>>>> 4f58a84b4c6f859324fe18e512c71cf1eb989ac3
     // #ifndef ONLINE_JUDGE  
         // freopen("input.txt", "r", stdin); 
         // freopen("output.txt", "w", stdout); 
     // #endif 
-<<<<<<< HEAD
     fastio;
     int t=1;
     cin>>t;
@@ -69,15 +48,3 @@ int main(){
         cout<<"Case #"<<_<<": ",solve(),_++;
     return 0;
 }
-=======
-    // fastio;
-    int t=1;
-    cin>>t;
-    int i=1;
-    while(t--){
-        cout<<"Case #"<<i<<": "<<solve()<<'\n';
-        i++;
-    }
-    return 0;
-}
->>>>>>> 4f58a84b4c6f859324fe18e512c71cf1eb989ac3
