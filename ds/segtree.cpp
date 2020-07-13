33fe46d5ae6149b0311b978da00103cafa07ddfc
Shim1998 <98shim@gmail.com>
2020-07-13 18:49:48 +0530

    Improved Segment Tree

--- ds/segtree.cpp
+++ ds/segtree.cpp
@@ -36,41 +36,70 @@ const int INF = 1e9+5;
 const int MOD = 1e9+7;
 double pi = 2 * acos(0.0); 
 
-vll segtree;
+//Codeforces EDU Step 1 practise solutions Basic Sum Over Segment Tree,RMQ, RMQ with counts
+
+struct dat{
+   ll s;
+   int val,cnt;
+};
+
+dat emin(dat a,dat b){
+   if(a.val<b.val) return a;
+   if(b.val<a.val) return b;
+   return {a.s,a.val,a.cnt+b.cnt};
+}
+
+vector<dat>segtree;
 
 int init(int n,int size){
    while(size<n) size*=2;
-   segtree.assign(2*size,0LL);
+   segtree.resize(2*size);
    return size;
 }
 
 void build(vi &a,int x,int lx,int rx){
    if(rx-lx==1){
-      if(lx<(int)a.size()) segtree[x]=a[lx];
+      if(lx<(int)a.size()) segtree[x].s=a[lx],segtree[x].val=a[lx],segtree[x].cnt=1;
    }
    else{
       int m=(lx+rx)/2;
       build(a,x*2+1,lx,m);
       build(a,x*2+2,m,rx);
-      segtree[x]=segtree[x*2+1]+segtree[x*2+2];
+      segtree[x].s=segtree[x*2+1].s+segtree[x*2+2].s;
+      segtree[x]=emin(segtree[x*2+1],segtree[x*2+2]);
    }
 }
 
 void update(int i,int v,int x,int lx,int rx){
-   if(rx-lx==1) segtree[x]=v;
+   if(rx-lx==1) segtree[x].s=v,segtree[x].val=v,segtree[x].cnt=1;
    else{
       int m=(lx+rx)/2;
       if(i<m) update(i,v,2*x+1,lx,m);
       else update(i,v,2*x+2,m,rx);
-      segtree[x]=segtree[x*2+1]+segtree[x*2+2];
+      segtree[x].s=segtree[x*2+1].s+segtree[x*2+2].s;
+      segtree[x]=emin(segtree[x*2+1],segtree[x*2+2]);
    }
 }
 
 ll sum(int l,int r,int x,int lx,int rx){
    if(lx>=r||l>=rx) return 0;
-   if(lx>=l&&rx<=r) return segtree[x];
+   if(lx>=l&&rx<=r) return segtree[x].s;
    int m=(lx+rx)/2;
    return sum(l,r,x*2+1,lx,m)+sum(l,r,x*2+2,m,rx);
+}
+
+int query1(int l,int r,int x,int lx,int rx){
+   if(lx>=r||l>=rx) return INF;
+   if(lx>=l&&rx<=r) return segtree[x].val;
+   int m=(lx+rx)/2;
+   return min(query1(l,r,x*2+1,lx,m),query1(l,r,x*2+2,m,rx));
+}
+
+dat query2(int l,int r,int x,int lx,int rx){
+   if(lx>=r||l>=rx) return {0,INF,1};
+   if(lx>=l&&rx<=r) return segtree[x];
+   int m=(lx+rx)/2;
+   return emin(query2(l,r,x*2+1,lx,m),query2(l,r,x*2+2,m,rx));
 }
 
 void solve(){
@@ -81,10 +110,11 @@ void solve(){
    rep(i,0,n) cin>>a[i];
    build(a,0,0,size);
    int o,l,r;
+   dat ans;
    while(m--){
       cin>>o>>l>>r;
       if(o==1) update(l,r,0,0,size);
-      else cout<<sum(l,r,0,0,size)<<'\n';
+      else ans=query2(l,r,0,0,size),cout<<ans.val<<' '<<ans.cnt<<'\n';
    }
 }
 
