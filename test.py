302a17fdfa39cdab1b0f4c9718b7b52a2f394731
Shivam Solanki <98shim@gmail.com>
2021-08-26 10:46:38 +0530

    removed .vscode

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-n=int(input())
-a=list(map(int,input().split()))
-x=0
-for i in range(len(a)-2):
-    x^=a[i]
-print(x)
-while(len(a)!=1):
-    for i in range(len(a)-1):
-        a[i]=a[i]^a[i+1]
-    a.pop()
-    print(a)
+string = input("Please Enter your Own String : ")
+alphabets = digits = spaces = 0
+
+for i in range(len(string)):
+    if((string[i]>='a' and string[i]<='z') or (string[i]>='A' and string[i]<='Z')):
+        alphabets = alphabets + 1
+    elif((string[i]>='0' and string[i]<='9')):
+        digits = digits + 1
+    elif(string[i]==' '):
+        spaces = spaces + 1
+        
+print("\nTotal Number of Alphabets in this String :  ", alphabets)
+print("Total Number of Digits in this String :  ", digits)
+print("Total Number of Special Characters in this String :  ", spaces)
