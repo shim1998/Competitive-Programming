4f402910938a8636f2f28cdcda3103b3040a2812
Omkar Deshmukh <omkarvd7066@gmail.com>
2021-09-30 22:44:34 +0530

    Formatted Codes

--- template.cpp
+++ template.cpp
@@ -1,6 +1,6 @@
 /*Made by Shivam Solanki*/
-#include<bits/stdc++.h>
-#pragma GCC optimize ("Ofast")
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
 using namespace std;
 #define DEBUG(x) cout << '>' << #x << ':' << x << endl;
 #define ll long long int
@@ -8,47 +8,50 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<vll> vvl;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef vector<vi> vvi;
 typedef vector<bool> vb;
 typedef vector<pii> vp;
 typedef vector<pll> vpll;
-typedef map<int,int> mii;
-typedef map<ll,ll> mll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
 typedef set<int> sii;
 typedef set<ll> sll;
 typedef queue<int> qii;
 typedef priority_queue<int> pq;
-typedef unordered_map<int,int> umii;
-typedef unordered_map<ll,ll> umll;
-#define all(x) x.begin(),x.end()
-#define rep(i,k,n) for (int i = k; i < n; ++i) 
-#define repr(i,k,n) for (int i = n; i>=k; --i)
-#define repll(i,k,n) for (ll i = k; i < n; ++i) 
+typedef unordered_map<int, int> umii;
+typedef unordered_map<ll, ll> umll;
+#define all(x) x.begin(), x.end()
+#define rep(i, k, n) for (int i = k; i < n; ++i)
+#define repr(i, k, n) for (int i = n; i >= k; --i)
+#define repll(i, k, n) for (ll i = k; i < n; ++i)
 #define pb push_back
 #define mp make_pair
 #define gcd __gcd
 #define F first
 #define S second
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-const int INF = 1e9+5;
-const int MOD = 1e9+7;
-double pi = 2 * acos(0.0); 
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+const int INF = 1e9 + 5;
+const int MOD = 1e9 + 7;
+double pi = 2 * acos(0.0);
 
-void solve(){
+void solve() {
 }
-    
-int main(){ 
-    // #ifndef ONLINE_JUDGE  
-        // freopen("input.txt", "r", stdin); 
-        // freopen("output.txt", "w", stdout); 
-    // #endif 
+
+int main() {
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
     fastio;
-    int t=1;
-    cin>>t;
-    int _=1;
-    while(t--)
-        cout<<"Case #"<<_<<": ",solve(),cout<<'\n',_++;
+    int t = 1;
+    cin >> t;
+    int _ = 1;
+    while (t--)
+        cout << "Case #" << _ << ": ", solve(), cout << '\n', _++;
     return 0;
 }
